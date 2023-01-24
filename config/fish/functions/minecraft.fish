6c8ba1cc11b39eac924a7bb4227057d36430706b
Henry-Hiles <henry@henryhiles.com>
2023-01-24 15:16:43 -0500

    remove unneccicary functions

--- config/fish/functions/minecraft.fish
+++
@@ -1,3 +0,0 @@
-function minecraft --wraps='kitty +kitten ssh root@192.168.1.191' --description 'alias minecraft=kitty +kitten ssh root@192.168.1.191'
-  kitty +kitten ssh root@192.168.1.191 $argv; 
-end
--- config/fish/functions/vm.fish
+++
@@ -1,3 +0,0 @@
-function vm --wraps='kitty +kitten ssh moose@107.174.70.14' --wraps='kitty +kitten ssh moose@107.174.70.140' --description 'alias vm=kitty +kitten ssh moose@107.174.70.140'
-  kitty +kitten ssh moose@107.174.70.140 $argv; 
-end
